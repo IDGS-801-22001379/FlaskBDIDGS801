5829dcb71ec9479dadd2f935af361411fe5ffc72
IDGS-801-22001379 <yaelxdmt88xfire07@gmail.com>
2025-03-03 23:42:33 -0600

    Detalles 03/03/2025

--- app.py
+++ app.py
@@ -7,18 +7,47 @@ from models import db, Alumnos
 # Inicializar Flask
 app = Flask(__name__)
 
+
 app.config.from_object(DevelopmentConfig)
+
 
 csrf = CSRFProtect(app)
 
+
 db.init_app(app)
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/index", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    form = forms.UserForm2(request.form)  # Corrección en la creación del formulario
+    alumnos = Alumnos.query.all()  # Recuperar los alumnos de la BD
+    return render_template("index.html", form=form, alumnos=alumnos)  # Corrección en la clave del contexto
+
+
+
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+    if request.method == 'GET':
+        id = request.args.get('id')
+
+      
+        print(f"ID recibido: {id}")
+
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+
+        if alum1:
+            nombre = alum1.nombre
+            apaterno = alum1.apaterno
+            email = alum1.email
+        else:
+            nombre = "No encontrado"
+            apaterno = "No encontrado"
+            email = "No disponible"
+
+    return render_template("detalles.html", id=id, nombre=nombre, apaterno=apaterno, email=email)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  
     app.run(debug=True)
+
